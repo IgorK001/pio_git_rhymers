54a53d7b0a02ead6db0b9be61847f770e181bb77
Igor Knop <240707@edu.p.lodz.pl>
2023-05-10 13:22:33 +0200

    15 walidajca projektu 17. generacje szkieletu dokumentacji

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,19 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * class with methods using stack approach
+ */
 public class IntLinkedList {
     public static final int RETURN_VALUE = -1;
 
     Node last;
     int i;
 
+    /**
+     *
+     * @param i
+     * push number i and put it on top of the stack
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -16,20 +24,36 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * @return
+     * return boolean value 1 - if stack is empty 0 if not
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * @return
+     * return boolen 0-not full
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * @return
+     * return value of element in top of the stack
+     */
     public int top() {
         if (isEmpty())
             return RETURN_VALUE;
         return last.value;
     }
 
+    /**
+     * @return
+     * pop top value from stack if its empty return RETURN_VALUE(-1)
+     */
     public int pop() {
         if (isEmpty())
             return RETURN_VALUE;
